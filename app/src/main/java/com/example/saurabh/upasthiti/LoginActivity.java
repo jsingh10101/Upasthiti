f157849ea520b75947bff2b147d51816f09a791a
JAPNEET SINGH <japneet.theultimateenlightment@gmail.com>
2018-11-01 03:37:41 +0530

    commited by Japneet Singh

--- app/src/main/java/com/example/saurabh/upasthiti/LoginActivity.java
+++ app/src/main/java/com/example/saurabh/upasthiti/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.example.saurabh.upasthiti;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -24,66 +25,86 @@ public class LoginActivity extends AppCompatActivity {
     Button log;
     EditText username, pwd;
     Connection con;
-    String un,pass,db,ip;
+    String un, pass, db, ip;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        log =(Button)findViewById(R.id.id_login);
-        username=(EditText)findViewById(R.id.edt1);
-        pwd=(EditText)findViewById(R.id.edt2);
-        un="root";
-        pass="root";
-        db="GKV2016";
-        ip="13.127.144.24:49170";
+        log = (Button) findViewById(R.id.id_login);
+        username = (EditText) findViewById(R.id.edt1);
+        pwd = (EditText) findViewById(R.id.edt2);
+        un = "root";
+        pass = "root";
+        db = "GKV2016";
+        ip = "13.127.144.24:49170";
         log.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                new ConSql().execute("");
+                ConSql dd = new ConSql();
+                dd.execute("");
             }
         });
 
     }
-    class ConSql extends AsyncTask<String,String,String>{
+
+    private class ConSql extends AsyncTask<String, String, Connection> {
+        private String un, pass, db, ip;
+
+
+        public ConSql() {
+            un = "root";
+            pass = "root";
+            db = "GKV2016";
+            ip = "13.127.144.24:49170";
+        }
+
         @Override
-        protected String doInBackground(String... strings) {
+        protected Connection doInBackground(String... strings) {
             String ConnectionURL;
             try {
                 Class.forName("net.sourceforge.jtds.jdbc.Driver");
-                ConnectionURL="jdbc:jtds:sqlserver://"+ip+";DatabaseName="+db+";user="+un+";password="+pass+";";
+                ConnectionURL = "jdbc:jtds:sqlserver://" + ip + ";DatabaseName=" + db + ";user=" + un + ";password=" + pass + ";";
 
-                con= DriverManager.getConnection(ConnectionURL);
-                if(con.isClosed())
-                    Toast.makeText(getBaseContext(),"Connection Failed",Toast.LENGTH_SHORT).show();
-                String empuser= username.getText().toString();
+                con = DriverManager.getConnection(ConnectionURL);
+                String empuser = username.getText().toString();
                 String emppass = pwd.getText().toString();
-                String query = "select CODE from GKV2016.dbo.EMPMASTER WHERE CODE = \'"+empuser+"\' AND PWD = \'"+emppass+"\';";
+                String query = "select NAME from GKV2016.dbo.EMPMASTER WHERE CODE = \'" + empuser + "\' AND PWD = \'" + emppass + "\';";
 
                 Statement st = con.createStatement();
                 ResultSet rs = st.executeQuery(query);
-
-                if(rs.next())
-                {
-                    Intent obj = new Intent(LoginActivity.this,MainActivity.class);
+                if (rs.next()) {
+                    final String name = rs.getString(1);
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(getBaseContext(), "Welcome Mr." + name, Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                    Intent obj = new Intent(LoginActivity.this, MainActivity.class);
+                    obj.putExtra("code", empuser);
                     startActivity(obj);
+                } else {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(getBaseContext(), "user not found", Toast.LENGTH_SHORT).show();
+                        }
+                    });
                 }
-                else
-                    {
-                        Toast.makeText(LoginActivity.this,"user not found",Toast.LENGTH_LONG).show();
-                    }
 
-                    rs.close();
+                rs.close();
                 st.close();
                 con.close();
-
-            }catch (Exception e)
-            {
-                Log.e("Error",""+e.getMessage());
+            } catch (Exception e) {
+                Log.e("Error", e.getMessage());
+                e.printStackTrace();
             }
             return null;
         }
     }
 
 }
+
--- app/src/main/java/com/example/saurabh/upasthiti/MainActivity.java
+++ app/src/main/java/com/example/saurabh/upasthiti/MainActivity.java
@@ -1,5 +1,9 @@
 package com.example.saurabh.upasthiti;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
@@ -7,20 +11,41 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.sql.Blob;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class MainActivity extends AppCompatActivity {
 
     private DrawerLayout mDrawerLayout;
     private ActionBarDrawerToggle mToggle ;
+    Connection con;
+    TextView name,post,facl,dept;
+    ImageView lim,him;
+    String code;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar=(Toolbar) findViewById(R.id.toolbar);
+        name=findViewById(R.id.tname);
+        post=findViewById(R.id.tpost);
+        facl=findViewById(R.id.tfacl);
+        dept=findViewById(R.id.tdept);
         setSupportActionBar(toolbar);
-
+        lim=findViewById(R.id.limage);
+        him=findViewById(R.id.himage);
+        code=getIntent().getStringExtra("code");
+        new ConSql().execute("");
         mDrawerLayout =(DrawerLayout)findViewById(R.id.drawer);
         mToggle = new ActionBarDrawerToggle(this,mDrawerLayout,toolbar,R.string.open,R.string.close);
 
@@ -34,6 +59,7 @@ public class MainActivity extends AppCompatActivity {
                 return false;
             }
         });
+
 //        getActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
@@ -45,6 +71,73 @@ public class MainActivity extends AppCompatActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+    private class ConSql extends AsyncTask<String, String, Connection> {
+        private String un, pass, db, ip;
+
+
+        public ConSql() {
+            un = "root";
+            pass = "root";
+            db = "GKV2016";
+            ip = "13.127.144.24:49170";
+        }
+
+        @Override
+        protected Connection doInBackground(String... strings) {
+            String ConnectionURL;
+            try {
+                Class.forName("net.sourceforge.jtds.jdbc.Driver");
+                ConnectionURL = "jdbc:jtds:sqlserver://" + ip + ";DatabaseName=" + db + ";user=" + un + ";password=" + pass + ";";
+
+                con = DriverManager.getConnection(ConnectionURL);
+
+                String query = "SELECT NAME,POST,FACULTY,DEPT,PHOTO FROM GKV2016.dbo.EMPMASTER WHERE CODE=\'"+code+"\';";
+                Statement st = con.createStatement();
+                final ResultSet rs = st.executeQuery(query);
+                if (rs.next()) {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            try {
+                                name.setText(rs.getString("NAME"));
+                                post.setText(rs.getString("POST"));
+                                facl.setText(rs.getString("FACULTY"));
+                                dept.setText(rs.getString("DEPT"));
+                                Blob bs=rs.getBlob("PHOTO");
+                                if(bs!=null){
+                                byte[] bytes=bs.getBytes(1,(int)bs.length());
+                                bs.free();
+                                Bitmap ss= BitmapFactory.decodeByteArray(bytes,0,bytes.length);
+                                lim.setImageBitmap(ss);
+                                him.setImageBitmap(ss);}
+                            }
+                            catch (Exception e) {
+                                Log.e("Error", e.getMessage());
+                                e.printStackTrace();
+                            }
+                        }
+                    });
+
+
+                } else {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(getBaseContext(), "user not found", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }
+
+                rs.close();
+                st.close();
+                con.close();
+            } catch (Exception e) {
+                Log.e("Error", e.getMessage());
+                e.printStackTrace();
+            }
+            return null;
+        }
     }
 
 }
