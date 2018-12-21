3714fd5274f2df481e1409cfbf50636ad8699552
JAPNEET SINGH <japneet.theultimateenlightment@gmail.com>
2018-12-21 18:59:33 +0530

    commited by Japneet Singh

--- app/src/main/java/com/example/saurabh/upasthiti/MainActivity.java
+++ app/src/main/java/com/example/saurabh/upasthiti/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
+import android.os.StrictMode;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
@@ -28,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
     private DrawerLayout mDrawerLayout;
     private ActionBarDrawerToggle mToggle ;
     Connection con;
-    TextView name,post,facl,dept;
+    TextView name,post,facl,dept,phone;
     ImageView lim,him;
     String code;
 
@@ -42,16 +43,56 @@ public class MainActivity extends AppCompatActivity {
         facl=findViewById(R.id.tfacl);
         dept=findViewById(R.id.tdept);
         setSupportActionBar(toolbar);
+        phone=findViewById(R.id.tphone);
         lim=findViewById(R.id.limage);
         him=findViewById(R.id.himage);
         code=getIntent().getStringExtra("code");
-        new ConSql().execute("");
         mDrawerLayout =(DrawerLayout)findViewById(R.id.drawer);
         mToggle = new ActionBarDrawerToggle(this,mDrawerLayout,toolbar,R.string.open,R.string.close);
-
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
         mDrawerLayout.addDrawerListener(mToggle);
         mToggle.syncState();
+        ConSql s=new ConSql();
+        String query = "SELECT NAME,POST,MOBILE,FACULTY,DEPT,PHOTO FROM GKV2016.dbo.EMPMASTER WHERE CODE=\'"+code+"\';";
+        s.execute(query);
+        try {
+            final ResultSet rs=s.get();
+            Log.v("Column Count",""+rs.getMetaData().getColumnCount());
+            if(rs.next())
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            Toast.makeText(getBaseContext(), rs.getString("NAME")+" "+rs.getMetaData().getColumnCount(), Toast.LENGTH_SHORT).show();
+                            name.setText(rs.getString("NAME"));
+                            post.setText(rs.getString("POST"));
+                            phone.setText(rs.getString("MOBILE"));
+                            facl.setText(rs.getString("FACULTY"));
+                            dept.setText(rs.getString("DEPT"));
+                            Blob bs=rs.getBlob("PHOTO");
+                            if(bs!=null){
+                                byte[] bytes=bs.getBytes(1,(int)bs.length());
+//                                bs.free();
+                                Bitmap ss= BitmapFactory.decodeByteArray(bytes,0,bytes.length);
+                                lim.setImageBitmap(ss);
+                                //him.setImageBitmap(ss);
+                            }
+                        }
+                        catch (Exception e) {
+                            Log.e("Error", e.getMessage());
+                            e.printStackTrace();
+                        }
+                    }
+                });
 
+
+
+            rs.close();
+        }catch (Exception e)
+        {
+            e.printStackTrace();
+        }
         NavigationView navigationView= (NavigationView) findViewById(R.id.id_navigation_view);
         navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
@@ -72,7 +113,7 @@ public class MainActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
-    private class ConSql extends AsyncTask<String, String, Connection> {
+   /* private class ConSql extends AsyncTask<String, String, Connection> {
         private String un, pass, db, ip;
 
 
@@ -125,7 +166,7 @@ public class MainActivity extends AppCompatActivity {
                         @Override
                         public void run() {
                             Toast.makeText(getBaseContext(), "user not found", Toast.LENGTH_SHORT).show();
-                        }
+                         }
                     });
                 }
 
@@ -138,6 +179,6 @@ public class MainActivity extends AppCompatActivity {
             }
             return null;
         }
-    }
+    */
 
 }
